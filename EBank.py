faf0ec409e16b99f15b383ded0f0e447261253da
焦点 <751682071@qq.com>
2018-01-22 01:07:40 +0800

    Submit my code for the first time.

---
+++ EBank.py
@@ -0,0 +1,123 @@
+#!/bin/env python
+# _*_ coding:utf-8_*_
+import json
+import time
+
+
+def new_account(account_id, account_password):
+    account_file = open('bank_account.json', 'r')
+    if len(account_file.read()) != 0:
+        with open('bank_account.json', 'r') as f:
+            account_dict = json.load(f)
+        account_dict[account_id] = account_password
+        with open('bank_account.json', 'w') as f:
+            json.dump(account_dict, f)
+        initialize_account(account_id)
+    else:
+        account_dict = {}
+        account_dict[account_id] = account_password
+        with open('bank_account.json', 'w') as f:
+            json.dump(account_dict, f)
+        initialize_account(account_id)
+
+
+def check_account_exist(account_id):
+    account_file = open('bank_account.json', 'r')
+    if len(account_file.read()) != 0:
+        with open('bank_account.json', 'r') as f:
+            account_dict = json.load(f)
+            if account_dict.has_key(account_id) is True:
+                return True
+            else:
+                return False
+    else:
+        return False
+
+
+def log_account(account_id, account_password):
+    'log in an existing account,validity needed.'
+    if check_account_exist(account_id) is True:
+        with open('bank_account.json', 'r') as f:
+            account_dict = json.load(f)
+        if account_dict[account_id] == account_password:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def initialize_account(account_id):
+    account_info_file = open('account_info.json', 'r')
+    if len(account_info_file.read()) != 0:
+        with open('account_info.json', 'r') as f:
+            account_info_dict = json.load(f)
+        account_info_dict[account_id] = 15000
+        with open('account_info.json', 'w') as f:
+            json.dump(account_info_dict, f)
+    else:
+        account_info_dict = {}
+        account_info_dict[account_id] = 15000
+        with open('account_info.json', 'w') as f:
+            json.dump(account_info_dict, f)
+
+
+def query(account_id):
+    'query current account info.Log in needed.'
+    with open('account_info.json', 'r') as f:
+        account_info = json.load(f)
+    print (
+        'Here is your account info : Balance:%s;Bill:%s' % (account_info[account_id], 15000 - account_info[account_id]))
+
+
+def spend(account_id, password, amount):
+    'for external requests.Balance verification needed.'
+    return
+
+
+def withdraw(account_id, amount):
+    'balance verification needed.'
+    with open('account_info.json', 'r') as f:
+        account_info_dict = json.load(f)
+    if account_info_dict[account_id] - amount < 0:
+        print "Sorry you have insufficient balance"
+        return False
+    else:
+        account_info_dict[account_id] -= amount
+        print 'Withdrew successfully!'
+        with open('account_info.json', 'w') as f:
+            json.dump(account_info_dict, f)
+        return True
+
+
+def bookkeeping_withdraw(account_id, amount):
+    'take every expense down'
+    account_book = open('%s.json' % account_id, 'w+')
+    if len(account_book.read()) != 0:
+        with open('%s.json' % account_id, 'r') as f:
+            account_book_dict = json.load(f)
+        bookkeeping_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        account_book_dict[bookkeeping_time] = [' Withdrew: ', amount]
+        with open('%s.json'%account_id, 'a') as f:
+            json.dump(account_book_dict[bookkeeping_time], f)
+        return True
+    else:
+        account_book_dict = {}
+        bookkeeping_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        account_book_dict[bookkeeping_time] = [' Withdrew: ', amount]
+        with open('%s.json'%account_id, 'w') as f:
+            json.dump(account_book_dict[bookkeeping_time], f)
+        return True
+
+
+def bill_generate():
+    'genernate a bill list every 30th of a month'
+    return
+
+
+def payback():
+    'pay back bills,add up the balance.'
+
+
+def check_password(account_id, password):
+    return
---
+++ welcom_page.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+# _*_ coding=utf-8_*_
+import EBank
+
+todo_tup = ('Create a new account', 'Log into an existing account')
+for i, element in enumerate(todo_tup, start=1):
+    print i, todo_tup[i - 1]
+todo_choice = raw_input('Welcome to JD Bank!What do you expect to do?')
+while True:
+    if todo_choice == '1':
+        account_id = raw_input('Please input an account:')
+        is_account_exist = EBank.check_account_exist(account_id)
+        if is_account_exist is True:
+            print ('Account already exists,please change to a different account.')
+        else:
+            account_password = raw_input('Please input password:')
+            EBank.new_account(account_id, account_password)
+            break
+
+    elif todo_choice == '2':
+        account_id = raw_input('Please input your account:')
+        account_password = raw_input('Please input password:')
+        log_account_check = EBank.log_account(account_id, account_password)
+        if log_account_check is True:
+            todo_tup2 = ('Show my account information', 'Withdraw', 'Check my bills')
+            for i, element in enumerate(todo_tup2, start=1):
+                print i, todo_tup2[i - 1]
+            todo_choice2 = raw_input('Welcome %s!What do you expect to do?' % account_id)
+            if todo_choice2 == '1':
+                EBank.query(account_id)
+                break
+            elif todo_choice2 == '2':
+                amount = input('How much do you need to withdraw?')
+                withdraw_result = EBank.withdraw(account_id, amount)
+                if withdraw_result is True:
+                    EBank.bookkeeping_withdraw(account_id, amount)
+                    break
+            elif todo_choice2 == '3':
+                EBank.displaybills(account_id)
+                break
+            else:
+                print('Please input 1/2 or 3')
+        else:
+            print ('Wrong account or password')
+    else:
+        print ('Please input 1 or 2')
